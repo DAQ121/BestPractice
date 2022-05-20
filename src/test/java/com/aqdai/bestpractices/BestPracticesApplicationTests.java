7d93b4cba03f76eaa1c855bb5fe1b6dd7d1b08f0
daiaoqi <aqdai@h-visions.com>
2022-05-20 16:44:51 +0800

    Initial commit

---
+++ src/test/java/com/aqdai/bestpractices/BestPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aqdai.bestpractices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestPracticesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
