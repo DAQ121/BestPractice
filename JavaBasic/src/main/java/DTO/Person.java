44087f22fc2b59525acb5ba58a77879f6f9b4978
daiaoqi <aqdai@h-visions.com>
2022-08-12 00:01:23 +0800

    Beanutils使用

--- JavaBasic/src/main/java/DTO/Person.java
+++ JavaBasic/src/main/java/DTO/Person.java
@@ -3,6 +3,8 @@ package DTO;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.List;
+
 /**
  * grade<p>Title: Student</p>
  * <p>Description: 学生实体类</p>
@@ -14,17 +16,7 @@ import lombok.Data;
  */
 
 @Data
-@Builder
-public class Student {
-    public Student() {
-    }
-
-    public Student(String name, Integer age, String sex, Integer grade) {
-        this.name = name;
-        this.age = age;
-        this.sex = sex;
-        this.grade = grade;
-    }
+public class Person {
 
     public String name;
 
@@ -33,5 +25,9 @@ public class Student {
     public String sex;
 
     public Integer grade;
+
+    public Integer money;
+
+    public List<Student> products;
 
 }
