4bb92edbb95e812b7c482ecf4b665ba7cf45cbc7
Eric <2829025551@qq.com>
2022-09-11 19:26:38 +0800

    Update GenericClass.java
    调整格式

--- JavaBasic/src/main/java/Generic/GenericClass.java
+++ JavaBasic/src/main/java/Generic/GenericClass.java
@@ -10,21 +10,16 @@ package Generic;
  * @version :1.0.0
  */
 public class GenericClass<T> {
-
     private T key;
-
     public GenericClass(T key) {
         this.key = key;
     }
-
     public T getKey() {
         return key;
     }
-
     public void setKey(T key) {
         this.key = key;
     }
-
     @Override
     public String toString() {
         return "GenericClass{" +
